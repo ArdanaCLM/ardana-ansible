101987a3a3a624572a1cf1e1b21d5f09eaad1ff3
Gary Smith <gary.smith@suse.com>
2018-09-20 11:00:13 -0700

    SCRD-4988 Obtain notification URL from environment
    Obtain the URL to the ardana service from the environment variable.
    Trying to use the host vars is problematic since the host vars in the
    pre-deployment directory (~/openstack) does not contain the ardana
    service URL.
    
    Depends-On: I706b2270c9aeee3555182eacf97e477e458e6ac5
    Change-Id: I5bd9d8c1cedc55807478953f99dd8d99585a15b1

--- callback_plugins/report_playbook_status.py
+++ callback_plugins/report_playbook_status.py
@@ -1,57 +1,21 @@
 # (c) Copyright 2017-2018 SUSE LLC
 
+import json
 import os
 import urllib2
-import json
-from contextlib import closing
-import socket
-import sys
-if sys.version_info.major < 3:
-    from urlparse import urlparse
-else:
-    from urllib.parse import urlparse
+
 
 class CallbackModule(object):
 
-    """
-    callback plugin sends events to the ardana-service so that other components
-    can listen to ansible events, particular playbook starts/stops
-
-    """
-    def __init__(self):
-        self.srvcurl = 'http://localhost:9085'
-        self.timeout = 5  # 5 seconds
-        # ardana-service runs on the deployer:9085 by default
-
-    def updateSrvcUrl(self):
-        """ Updates the service url to one advertised by the VIP
-            but only if the corresponding port is connectable
-        """
-        hosts = self.playbook.inventory.get_hosts()
-        if(hosts):
-            try:
-                host_vars = \
-                    self.playbook.inventory.get_variables(hosts[0].name)
-                url = \
-                    host_vars['ARD_SVC']['advertises'][
-                        'vips']['private'][0]['url']
-                purl = urlparse(url)
-                with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
-                    s.connect((purl.hostname, purl.port))
-                    self.srvcurl = url
-            except (KeyError, TypeError, socket.error) as error:
-                # leave the existing URL in place if the key isnt found
-                pass
-
+    # Callback plugin sends events to the ardana-service so that other
+    # components can listen to ansible events, particular playbook starts/stops
     def playbook_on_task_start(self, name, is_conditional):
-        """Triggers callbacks other services if a specific play was run
-          Since playbooks are being run in a nested fashion, only
-          the top level playbook will trigger playbook_on_stats
-          indicating that its finished (the same is true for
-          playbook_on_start). Instead, fake the start/finish
-          events by injecting a start and finish task into
-          important playbooks, and parse for that task here
-        """
+        # Triggers callbacks other services if a specific play was run Since
+        # playbooks are being run in a nested fashion, only the top level
+        # playbook will trigger playbook_on_stats indicating that its finished
+        # (the same is true for playbook_on_start). Instead, fake the
+        # start/finish events by injecting a start and finish task into
+        # important playbooks, and parse for that task here
         callbacks_map = {
             'pbfinish.yml pb_finish_playbook': 'stop',
             'pbstart.yml pb_start_playbook': 'start'
@@ -74,20 +38,20 @@ class CallbackModule(object):
         self.post_to_listener(self.playbook.filename, action)
 
     def post_to_listener(self, playbook, action):
-        self.updateSrvcUrl()
-        urlpath = '/api/v2/listener/playbook'
-        url = self.srvcurl + urlpath
-        try:
-            data = json.dumps({
-                'play_id': os.environ['PLAY_ID'],
-                'event': action,
-                'playbook': playbook
-             })
-            req = urllib2.Request(url, data,
-                                  {'Content-type': 'application/json'})
-            f = urllib2.urlopen(req)
-            response = f.read()
-        except Exception as ex:
-            # nothing to do on exception, probably means the URL is incorrect
-            # or not available for the playbook in question
-            pass
+        if 'NOTIFY_URL' in os.environ and 'PLAY_ID' in os.environ:
+            urlpath = '/api/v2/listener/playbook'
+            url = os.environ['NOTIFY_URL'] + urlpath
+            try:
+                data = json.dumps({
+                    'play_id': os.environ['PLAY_ID'],
+                    'event': action,
+                    'playbook': playbook
+                })
+                req = urllib2.Request(url, data,
+                                      {'Content-type': 'application/json'})
+                f = urllib2.urlopen(req)
+                f.read()
+            except Exception:
+                # nothing to do on exception, probably means the URL is
+                # incorrect or not available for the playbook in question
+                pass
